bb4f0481579cc0c70357ff6b0c79e73a777ab3b7
Mohamed Hnef <mohamed.hnef1@gmail.com>
2024-07-14 00:28:19 +0300

    Initial server setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const preferencesRoutes = require('./routes/preferencesRoutes');
+const vacationRoutes = require('./routes/vacationRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
+const db = require('./db');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/preferences', preferencesRoutes);
+app.use('/api/vacation', vacationRoutes);
+app.use('/api/weather', weatherRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = db;
