4e94c7a1a147c67000d47b09d99cf31cab38b727
Mohamed Hnef <mohamed.hnef1@gmail.com>
2024-07-15 13:29:50 +0300

    fixed registeration with same username

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,19 +9,30 @@ exports.register = (req, res) => {
     return res.status(400).json({ message: 'Username and password are required' });
   }
 
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) return res.status(500).json({ message: 'Error hashing password', error: err });
+  db.query('SELECT * FROM tbl_42_users WHERE username = ?', [username], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Database error', error: err });
+    }
 
-    db.query('INSERT INTO tbl_42_users (username, password) VALUES (?, ?)', [username, hash], (err, results) => {
-      if (err) return res.status(500).json({ message: 'Database error', error: err });
+    if (results.length > 0) {
+      return res.status(400).json({ message: 'Username already taken' });
+    }
 
-      const payload = { username }; 
-      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '10h' });
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) return res.status(500).json({ message: 'Error hashing password', error: err });
 
-      res.status(201).json({ message: 'User registered successfully', accessCode: token });
+      db.query('INSERT INTO tbl_42_users (username, password) VALUES (?, ?)', [username, hash], (err, results) => {
+        if (err) return res.status(500).json({ message: 'Database error', error: err });
+
+        const payload = { username }; 
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '10h' });
+
+        res.status(201).json({ message: 'User registered successfully', accessCode: token });
+      });
     });
   });
 };
+
 
 exports.login = (req, res) => {
   const { username, password } = req.body;
